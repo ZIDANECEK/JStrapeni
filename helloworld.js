b6baf244a0d50b27eca65a9b7ec6a875073715cc
ZIDANEK <bobalamartin@gmail.com>
2023-03-01 18:18:01 +0100

    nepiju

--- helloworld.js
+++ helloworld.js
@@ -1,17 +1,14 @@
-const calculate = (H, S, Y) => {
-  let X = ((H-S) /2 + S) * Math.PI / Y
-  alert(X)
-  setTimeout(getValues,20)
-  }
-  
-  
-  function getValues() {
-    const H = prompt("Enter H as Number")
-    const S = prompt("Enter S as Number")
-    const Y = prompt("Enter Y as Number")
-
-   calculate(+H, +S, +Y)
-  }
+let button =  document.getElementById("button1") 
+button.onclick = clicked => alert(arr[Math.floor(Math.random() * 11)])
 
 
-  setTimeout(getValues,1000)
+const arr = ["Ale ja nepiju!",
+"Vodka to je také že si dáš jedno a FUJ!",
+"Když už můžu legálně tak proč bych si nedal.",
+"To pivko poslední týden se nepočítá.",
+"To byly narozky to se musí.",
+ "Fakt nepiju.",
+ "To vezmeš tak tu basu a načepujes kolik chceš.",
+ "Jo on chodi do Sportu ale je mu 18 to je děcko.",
+  "Ja jsem nikdy nepil!","No jsem si dal ale nedal doopravdy.",
+   "Jenom jedno a to uz si nepamatuju kdy.", ]
